a4d6e20e71149ee9a90522e1d0173ae2e4fff789
William <awaitfornemesis@gmail.com>
2021-06-17 17:07:23 +0500

    del default code app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
