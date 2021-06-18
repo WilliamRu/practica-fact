3d4f2a9e06de4ab7dc87e79f5f6cb7056d051b92
Nissko <skibko.nik@mail.ru>
2021-06-18 12:46:19 +0500

    f to pay respect

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-	<div id="app">
-    Hi! It`s my first page. I don`t know what i will write here.
+	<div id="app" class="Export">
+<!--    Hi! It`s my first page. I don`t know what i will write here.-->
+    Это полученная экспонента: {{exponent}}
+<!--    <button @click="initInterval"></button>-->
 	</div>
 </template>
-
 <script>
-
 export default {
-	name: "App",
-	components: {},
+  name: "App",
+  data(){
+    return {
+        value: 0,
+    };
+  },
+  computed:{
+    exponent(){
+      return Math.exp(this.value);
+    }
+  },
+  mounted() {
+    this.initInterval()
+  },
+  methods:{
+    initInterval(){
+      setInterval(() => this.value++, 1000);
+    }
+  },
 };
+
 </script>
 
 <style lang="scss">
+.Export{
+  color: blueviolet;
+  background-color: black;
+  height: 440px;
+  padding-top: 200px;
+  text-align: center;
+}
 </style>
