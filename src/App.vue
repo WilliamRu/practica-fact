f3b61fc35c78b9abc319a9aa4b8f20da4f722a98
Nissko <skibko.nik@mail.ru>
2021-06-21 14:51:24 +0500

    Nikita

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,68 @@
 <template>
-	<div id="app" class="Export">
-<!--    Hi! It`s my first page. I don`t know what i will write here.-->
-    Это полученная экспонента: {{exponent}}
-<!--    <button @click="initInterval"></button>-->
+	<div id="bot" class="chat-bot">
+    <header class="header-content">
+      <span class="close-bot"></span>
+      <div class="name-bots">Frog-Bot</div>
+    </header>
+    <main class="main-content">jj</main>
+    <footer class="footer-content">jj</footer>
 	</div>
 </template>
 <script>
 export default {
-  name: "App",
+  name: "bot",
   data(){
     return {
-        value: 0,
+
     };
   },
   computed:{
-    exponent(){
-      return Math.exp(this.value);
-    }
-  },
+
+    },
   mounted() {
-    this.initInterval()
+
   },
   methods:{
-    initInterval(){
-      setInterval(() => this.value++, 1000);
-    }
+
   },
 };
 
 </script>
 
 <style lang="scss">
-.Export{
-  color: blueviolet;
+.chat-bot{
+  /*background-color: #7c82ca;*/
+  margin:0 auto;
   background-color: black;
-  height: 440px;
-  padding-top: 200px;
-  text-align: center;
+  height: 500px;
+  width: 450px;
+  box-shadow:
+      0 1px 4px rgba(0, 0, 0, .3),
+      -23px 0 20px -23px rgba(0, 0, 0, .8),
+      23px 0 20px -23px rgba(0, 0, 0, .8),
+      0 0 40px rgba(0, 0, 0, .1) inset;
+
+}
+.header-content{
+  background-color: #7c82ca;
+  height: 100px;
+}
+.main-content{
+  background-color: floralwhite;
+  height: 300px;
+}
+.footer-content{
+  background-color: #7c82ca;
+  height: 100px;
+}
+.name-bots{
+  color: #ffffff;
+  font-size: 25px;
+  font-weight: bold;
+  padding-left: 95px;
+  padding-top: 35px;
+}
+.close-bot{
+  display: flex;
 }
 </style>
