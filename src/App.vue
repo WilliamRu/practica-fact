c84a63d5aff43b99b1f1a7bc193f80d4985ef41f
dorabyrda <darya.zhdanova.02@inbox.ru>
2021-06-22 18:33:29 +0500

    закинула чатик со стилями

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,31 @@
       <p class="bio">NO HORNY.</p>
       <p class="bio">only memes! & math</p>
     </header>
+
     <main class="main-content">
       <div class="main-content__message-area">
-        <div class="main-content__message-area-item"></div>
-
-
+        <div class="main-content__message-area-item message-bot">
+          <div class="main-content__message-area-message">Привет!</div>
+        </div>
+        <div class="main-content__message-area-item message-human">
+          <div class="main-content__message-area-message">Как дела?</div>
+        </div>
+        <div class="main-content__message-area-item message-bot">
+          <div class="main-content__message-area-message">Зашибок</div>
+        </div>
       </div>
     </main>
+
     <footer class="footer-content">
-      <img src="../frog.png" class="footer_logo">
+      <div class="Enter-Window">
+        <textarea class="input-style" maxlength="1500" placeholder="Введите сообщение"></textarea>
+        <button class="input-button" type="button"></button>
+
+      </div>
     </footer>
   </div>
 </template>
+
 <script>
 export default {
   name: "bot",
@@ -26,11 +39,19 @@ export default {
     return {};
   },
   computed: {},
-  methods: {},
   mounted() {
-    this.initInterval();
+    document.querySelector('textarea').addEventListener('input', function (e) {
+      if (e.target.style.height <= 100 || e.target.value.length <= 100) {
+        e.target.style.height = '1px';
+        e.target.style.height = e.target.scrollHeight + 30 + "px";
+      }
+      if (e.target.value.length === 0) {
+        e.target.style.height = '25px';
+      }
+    })
   },
-};
+  methods: {},
+}
 </script>
 
 <style lang="scss">
@@ -44,6 +65,8 @@ export default {
   -23px 0 20px -23px rgba(0, 0, 0, .8),
   23px 0 20px -23px rgba(0, 0, 0, .8),
   0 0 40px rgba(0, 0, 0, .1) inset;
+  font-family: 'system-ui';
+  color: white;
 }
 
 .header-content {
@@ -61,26 +84,57 @@ export default {
 }
 
 .main-content {
-  //background-color: floralwhite;
   background: no-repeat url(../back.png);
   background-size: 450px 500px;
   height: 500px;
-
   &__message-area {
+    display: flex;
+    flex: 1 0;
+    flex-direction: column;
     overflow-y: auto;
     width: 100%;
     max-height: 100%;
     height: 100%;
-
     &-item {
-      width: 100%;
-      height: 30px;
-      background: rgba(0, 0, 0, .1);
-      margin-bottom: 20px;
+      display: flex;
+      justify-content: center;
+      height: auto;
+      font-weight: bold;
+      width: 50%;
+      padding: 0.5rem 0.75rem;
+      flex-shrink: 0;
+      margin-bottom: 8px;
+      vertical-align: center;
+      text-align: center;
+      right:0;
+    }
+    .message-human{
+      color: rgba(245, 245, 245, 1);
+      background: radial-gradient(circle, rgba(0, 194, 10, .7), rgba(0, 181, 9, .7));
+      margin-left: 195px;
+      border-radius: 30px;
+      padding: 0.5rem 0.75rem;
+      -webkit-box-shadow: 0 5px 48px 2px rgba(34, 60, 80, 0.2) inset;
+      -moz-box-shadow: 0 5px 48px 2px rgba(34, 60, 80, 0.2) inset;
+      box-shadow: 0 5px 48px 2px rgba(34, 60, 80, 0.2) inset;
+    }
+    .message-bot{
+      color: rgba(245, 245, 245, 1);
+      background: radial-gradient(circle, rgba(148, 147, 143, .7), rgba(122, 122, 118, .7));
+      border-radius: 30px;
+      margin-left: 5px;
+      padding: 0.5rem 0.75rem;
+      -webkit-box-shadow: 0 5px 48px 2px rgba(34, 60, 80, 0.2) inset;
+      -moz-box-shadow: 0 5px 48px 2px rgba(34, 60, 80, 0.2) inset;
+      box-shadow: 0 5px 48px 2px rgba(34, 60, 80, 0.2) inset;
     }
   }
 }
-
+.main-content__message-area-message{
+  height: auto;
+  width: 90%;
+  word-break: break-all;
+}
 .footer-content {
   background-color: #7c82ca;
   height: 100px;
@@ -104,12 +158,78 @@ export default {
   //display: flex;
 }
 
-.footer_logo{
-  float: right;
-  height: 70px;
-  width: 70px;
-  border-radius: 50%;
-  margin-top: 15px;
-  margin-right: 10px;
+
+
+
+.input-style {
+  text-decoration: none;
+  resize: none;
+  border-radius: 15px;
+  width: 300px;
+  height: 25px;
+  margin-top: 28px;
+  outline: none;
+  padding-left: 15px;
+  padding-top: 10px;
+  padding-right: 10px;
+  overflow: auto;
 }
+
+.input-style::-webkit-input-placeholder {
+  opacity: 1;
+  transition: opacity 0.3s ease;
+}
+
+.input-style::-moz-placeholder {
+  opacity: 1;
+  transition: opacity 0.3s ease;
+}
+
+.input-style:-moz-placeholder {
+  opacity: 1;
+  transition: opacity 0.3s ease;
+}
+
+.input-style:-ms-input-placeholder {
+  opacity: 1;
+  transition: opacity 0.3s ease;
+}
+
+.input-style:focus::-webkit-input-placeholder {
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.input-style:focus::-moz-placeholder {
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.input-style:focus:-moz-placeholder {
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.input-style:focus:-ms-input-placeholder {
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.Enter-Window {
+  display: flex;
+  justify-content: center;
+}
+
+.input-button {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-top: 20px;
+  outline: none;
+  margin-left: 15px;
+  background: url("../frog.png") no-repeat center;
+  background-size: 50px;
+  border: none;
+}
+
 </style>
