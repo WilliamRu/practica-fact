05a3cf1115b29e99fdf43c44595fc792d2b851a3
kirina29 <kotlyar.29@mail.ru>
2021-06-28 13:28:53 +0500

    Nothing

--- src/App.vue
+++ src/App.vue
@@ -118,6 +118,7 @@ export default {
       }
       return null;
     },
+
   },
   computed: {},
 
--- src/components/MathActions.js
+++ src/components/MathActions.js
@@ -1,4 +1,4 @@
-const sum = (arr) => arr.reduce((accum, curr, ) => +accum + +curr);
+const sum = (arr) => arr.reduce((accum, curr) => +accum + +curr);
 const minus = (arr) => arr.reduce((accum, curr) => +accum - +curr);
 const mul = (arr) => arr.reduce((accum, curr) => +accum * +curr);
 const division = (arr) => arr.reduce((accum, curr) => +accum / +curr);
